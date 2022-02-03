be4e51c470cad4e0e3c9c3e06ac069c5442e4c9b
BijanSeif <b.sayyaf@yahoo.com>
2022-02-03 23:04:51 +0330

    MultiEl Function Update
    MultiElEQDOF Has been added to consider End pinned condition using EqualDOF command!

--- build/lib/topspy/dynamic/__init__.py
+++
@@ -1,3 +0,0 @@
-from topspy.dynamic.LAT2 import *
-from topspy.dynamic.RspSpc import *
-from topspy.dynamic.SDFTHA import *
--- build/lib/topspy/modeling/__init__.py
+++
@@ -1,5 +0,0 @@
-from topspy.modeling.eleAxialForce import *
-from topspy.modeling.ElePerPend import *
-from topspy.modeling.GmTVector import *
-from topspy.modeling.MultiEl import *
-
